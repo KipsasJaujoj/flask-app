b9ae7387cb7be26e5f9d033cdb6507be7ed9e685
KipsasJaujoj <vaidotasslavinskas@gmail.com>
2018-07-11 09:34:35 +0300

    added index.html

--- wsgi.py
+++ wsgi.py
@@ -2,8 +2,14 @@ from flask import Flask
 application = Flask(__name__)
 
 @application.route("/")
-def hello():
-    return "Hello World!"
-
+def index():	
+	time, temp, hum = getData()
+	templateData = {
+		'mac': "44:55:66:77:88:99",
+		'distance': 42.58,
+		'time': "09:33"
+	}
+	return render_template('index.html', **templateData)
+	
 if __name__ == "__main__":
     application.run()
