08622a5134281fbbbb6f1ab2820470ab5a0b91c8
KipsasJaujoj <vaidotasslavinskas@gmail.com>
2018-07-18 08:26:57 +0300

    test

--- wsgi.py
+++ wsgi.py
@@ -1,14 +1,23 @@
 from flask import Flask
 from flask import flash, render_template, request, redirect
+import MySQLdb
 application = Flask(__name__)
 
 @application.route("/")
 def index():	
-    templateData = {
-        'mac': "44:55:66:77:88:99",
-        'distance': 42.58,
-        'time': "09:33"
-    }
+    user = "remote-admin"
+    passwd = "Some-pass!23"
+    dbhost = "172.30.24.1"
+    dbname = "smart-recycling-bins"
+    db = MySQLdb.connect(host=dbhost, user=user, passwd=passwd, db=dbname)        
+    cur = db.cursor()
+    cur.execute("""SELECT * FROM `sensor_data` ORDER BY timestamp DESC LIMIT 1""")
+    for (mac, data, timestamp) in cur:
+	    templateData = {
+        'mac': mac,
+        'distance': data,
+        'time': timestamp
+    }   
     return render_template('index.html', **templateData)
 
 if __name__ == "__main__":
