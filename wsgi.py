fc339036e3c9479e87041cf31bbd5e6e4f11bbbc
KipsasJaujoj <vaidotasslavinskas@gmail.com>
2018-07-18 07:45:00 +0300

    test

--- wsgi.py
+++ wsgi.py
@@ -3,13 +3,12 @@ application = Flask(__name__)
 
 @application.route("/")
 def index():	
-#    time, temp, hum = getData()
     templateData = {
         'mac': "44:55:66:77:88:99",
         'distance': 42.58,
         'time': "09:33"
     }
-    return render_template('index.html')
+    return render_template('templates/index.html')
 
 if __name__ == "__main__":
     application.run(debug=True)
