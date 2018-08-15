e130ff438d3e57520406343df400d0ab7b10d2f2
KipsasJaujoj <vaidotasslavinskas@gmail.com>
2018-08-15 18:02:38 +0300

    generation of month data added

--- wsgi.py
+++ wsgi.py
@@ -20,7 +20,8 @@ def get_graph_data():
     time_scale = request.form.get("time_scale", "month")
     if time_scale == "month":
         min_date = datetime.now() - timedelta(days=30)
-    data = {"time_scale": min_date.strftime("%Y-%m-%d %H:%M:%S")}
+    data = get_template_data(min_date)
+    data = {"time_scale": data}
     data = jsonify(data)
     return data
 
@@ -28,32 +29,35 @@ def get_graph_data():
 def send_ota(path):
     return send_from_directory('static', path)
 
-def get_template_data(time_scale=None):
+def get_template_data(date=None):
     templateData = {}
     templateData['config'] = config
-    if time_scale is None:
-        user = "remote-admin"
-        passwd = "Some-pass!23"
-        dbhost = os.environ["MYSQL_SERVICE_HOST"]
-        dbname = "smart-recycling-bins"
-        db = MySQLdb.connect(host=dbhost, user=user, passwd=passwd, db=dbname)        
-        cur = db.cursor()
+    user = "remote-admin"
+    passwd = "Some-pass!23"
+    dbhost = os.environ["MYSQL_SERVICE_HOST"]
+    dbname = "smart-recycling-bins"
+    db = MySQLdb.connect(host=dbhost, user=user, passwd=passwd, db=dbname)        
+    cur = db.cursor()
 #        cur.execute("""SELECT * FROM `sensor_data` WHERE timestamp < '2018-08-06 09:38:40' ORDER BY timestamp DESC LIMIT 200""")
-        cur.execute("""SELECT * FROM `sensor_data` ORDER BY timestamp DESC LIMIT 500""")
-        data = cur.fetchall()
-        parsed_data = OrderedDict()
-        for (id, mac_id, distance, datetime_object) in data:
-            date_time = datetime_object.strftime("%Y-%m-%d %H:%M:%S")
-            if date_time in parsed_data:
-                parsed_data[date_time].update({ mac_id: { "distance": distance }})
-            else:
-                parsed_data.update({ date_time: { mac_id: { "distance": distance }}})
-        filtered_data = OrderedDict()
-        for i in parsed_data:
-            if len(parsed_data[i]) == 4:
-                filtered_data.update({i: parsed_data[i]})
-        templateData['distance_data'] = OrderedDict(sorted(filtered_data.items()))
-        return templateData
+    if date is None:
+        mysql_string = "SELECT * FROM `sensor_data` ORDER BY timestamp DESC LIMIT 500"
+    else:
+        mysql_string = "SELECT * FROM `sensor_data` WHERE timestamp < '{}' ORDER BY timestamp DESC".format(date)
+    cur.execute(mysql_string)
+    data = cur.fetchall()
+    parsed_data = OrderedDict()
+    for (id, mac_id, distance, datetime_object) in data:
+        date_time = datetime_object.strftime("%Y-%m-%d %H:%M:%S")
+        if date_time in parsed_data:
+            parsed_data[date_time].update({ mac_id: { "distance": distance }})
+        else:
+            parsed_data.update({ date_time: { mac_id: { "distance": distance }}})
+    filtered_data = OrderedDict()
+    for i in parsed_data:
+        if len(parsed_data[i]) == 4:
+            filtered_data.update({i: parsed_data[i]})
+    templateData['distance_data'] = OrderedDict(sorted(filtered_data.items()))
+    return templateData
     
 if __name__ == "__main__":
     application.run(debug=True)
